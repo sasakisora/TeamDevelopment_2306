71dccc4fd9f2d69d7050cbff3cd82d7009bd4ee6
sasakisora <109976240+sasakisora@users.noreply.github.com>
2023-06-12 21:22:23 +0900

    新規作成

---
+++ TeamDevelopment_2306/src/main/java/com/example/demo/TeamDevelopment2306Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamDevelopment2306Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamDevelopment2306Application.class, args);
+	}
+
+}
