dfb421c26acf269320629043cd6295dd4f3f52e7
kdmaoi <kdmaoi0118@gmail.com>
2023-06-27 00:45:41 +0900

    6/27

--- TeamDevelopment_2306/src/main/java/com/example/demo/entity/StudyRegisterEntity.java
+++ TeamDevelopment_2306/src/main/java/com/example/demo/entity/StudyRegisterEntity.java
@@ -1,5 +1,55 @@
 package com.example.demo.entity;
 
+import java.sql.Date;
+import java.sql.Time;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+/**
+ * 学習時間登録 Entity
+ */
+@Data
+@Entity
+@Table(name = "study_times")
+
 public class StudyRegisterEntity {
+
+	/**
+	 * ID
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	/**
+	 * ユーザーID
+	 */
+	@Column(name = "user_id")
+	private String userId;
+
+	/**
+	*日付
+	*/
+	@Column(name = "study_date")
+	private Date studyDate;
+
+	/**
+	*目標時間
+	*/
+	@Column(name = "target_date")
+	private Time targetTime;
+
+	/**
+	*学習時間
+	*/
+	@Column(name = "study_date")
+	private Time studyTime;
 
 }
--- TeamDevelopment_2306/src/main/java/com/example/demo/repository/StudyRegisterRepository.java
+++ TeamDevelopment_2306/src/main/java/com/example/demo/repository/StudyRegisterRepository.java
@@ -1,5 +1,10 @@
 package com.example.demo.repository;
 
-public class StudyRegisterRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-}
+/**
+*ユーザー情報　Repository
+*/
+@Repository
+public interface UserEditRepository extends JpaRepository<User, Long> {}
